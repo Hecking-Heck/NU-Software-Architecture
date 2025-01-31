8d5220fd5b735c3a5e5a3518c240659be3adc824
Heck <33295627+Hecking-Heck@users.noreply.github.com>
2025-01-31 13:13:17 +0000

    Update index.php

--- Assignment/index.php
+++ Assignment/index.php
@@ -1,13 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Assignment</title>
-</head>
-<body>
+<?php
 
-    <?php echo '<p>Hello World</p>'; ?>
-    
-</body>
-</html>
+# Simple 'Hello' script
+$name = "Evey"; # This is the name variable
+echo "<h1>Hello $name</h1>"; # This will output the name variable in HTML in a H1 tag
+
+# Things I want to do
+# Display Login Form
+# Make a weather API
