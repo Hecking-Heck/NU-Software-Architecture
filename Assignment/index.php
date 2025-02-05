de695f1cac4ab23f45fd622c180e24959cde5a5f
Heck <33295627+AidenM6901@users.noreply.github.com>
2025-02-05 00:14:41 +0000

    Update index.php

--- Assignment/index.php
+++ Assignment/index.php
@@ -1,9 +1,3 @@
 <?php
 
-# Simple 'Hello' script
-$name = "Evey"; # This is the name variable
-echo "<h1>Hello $name</h1>"; # This will output the name variable in HTML in a H1 tag
-
-# Things I want to do
-# Display Login Form
-# Make a weather API
+# This will be updated after the assessment has been marked.
